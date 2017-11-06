d93141f3b01a731d5eb3fbba0ed7f263a065b6b9
The-Promised-End <tom.tk.li@gmail.com>
2017-11-06 20:43:39 +0000

    chunk count

---
+++ client.js
@@ -0,0 +1,26 @@
+var https = require('https');
+
+function getAndPrintHTMLChunks () {
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+
+  https.get(requestOptions, function (response) {
+
+    response.setEncoding("utf8");
+
+    let responseBodyString = '';
+
+    response.on("data", function (chunk) {
+
+      console.log("chunk");
+
+      responseBodyString += chunk;
+    });
+  })
+
+};
+
+getAndPrintHTMLChunks();
